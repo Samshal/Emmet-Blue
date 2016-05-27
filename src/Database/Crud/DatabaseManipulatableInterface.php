41d7d7128e56b8490b4d890eabf18ce05ea6103f
nwalichukz <chukznwali@ggmail.com>
2016-05-27 07:34:07 +0100

    databasemanipulationInterface done

--- src/Database/Crud/DatabaseManipulatableInterface.php
+++ src/Database/Crud/DatabaseManipulatableInterface.php
@@ -9,12 +9,30 @@
 Namespace EmmetBlue\Database\Crud;
 
 /**
- * 
+  *This interface will be used to manipulate
+	*our database interactions/operations
  */
 interface DatabaseManipulatableInterface {
 
 	/**
-	 *
-	 */
+	 *The setData method collects the data to 
+	 * be manipulated in the database.
+	*/
+
+public function setData ();
+
+/**
+ *The CrudAction method performs either 
+ * create, retrieve, update or delete actions to data in the database.
+*/
+
+public function crudAction();
+
+   /**
+* The getResponse method returns the result of 
+* the crudAction method.
+  */
+public function getResponse();
+
 
 }
