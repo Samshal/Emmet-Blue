65d65a8b81013e850e2086d291dc5271fddba66a
Samshal <samueladeshina73@gmail.com>
2016-05-27 13:21:01 +0100

    EDITS, Impossible to document since I'm automating this process, so sorry;

---
+++ src/Database/Crud/Abstraction/QueryBuilder.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @license MIT
+ * @author Samuel Adeshina <samueladeshina73@gmail.com>
+ */
+
+namespace EmmetBlue\Database\Crud\Abstraction;
+
+
+class QueryBuilder implements QueryBuildableInterface {
+	
+}
