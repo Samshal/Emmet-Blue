84cbf902f197699ae9a30b01b2704587ce61d23b
Samshal <samueladeshina73@gmail.com>
2016-05-28 17:35:43 +0100

    Added tests for the values method in InsertQueryBuilder;

--- test.php
+++ test.php
@@ -2,7 +2,14 @@
 require __DIR__."/vendor/autoload.php";
 
 use \EmmetBlue\Database\Crud\Abstraction\InsertQueryBuilder;
+
 $qb = new InsertQueryBuilder();
 
-$qb = $qb->into("students", ["firstname", "lastname"])->values([[InsertQueryBuilder::wrapString("Samuel", "'"), "Adeshina"], ["John", "Doe"], ["Amos", 12]]);
-echo $qb;
+$sqlQuery = $qb
+			->into("students", ["firstname", "lastname"])
+			->values([
+				[InsertQueryBuilder::wrapString("sam", "'")],
+				[InsertQueryBuilder::wrapString("chukz", "'")],
+				[InsertQueryBuilder::wrapString("lucky", "'")]
+			]);
+echo $sqlQuery;
