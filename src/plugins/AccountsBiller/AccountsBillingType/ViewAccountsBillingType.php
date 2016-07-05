2b590adc6872651522f87f62429e983ac1865400
Samshal <samueladeshina73@gmail.com>
2016-07-05 17:20:29 +0100

    Created AccountsBiller

---
+++ src/plugins/AccountsBiller/AccountsBillingType/ViewAccountsBillingType.php
@@ -0,0 +1,77 @@
+<?php declare(strict_types=1);
+/**
+ * @license MIT
+ * @author Samuel Adeshina <samueladeshina73@gmail.calculhmac(clent, data)om>
+ *
+ * This file is part of the EmmetBlue project, please read the license document
+ * available in the root level of the project
+ */
+namespace EmmetBlue\Plugins\Mortuary\Body;
+
+use EmmetBlue\Core\Builder\BuilderFactory as Builder;
+use EmmetBlue\Core\Factory\DatabaseConnectionFactory as DBConnectionFactory;
+use EmmetBlue\Core\Factory\DatabaseQueryFactory as DatabaseQueryFactory;
+use EmmetBlue\Core\Builder\QueryBuilder\QueryBuilder as QB;
+use EmmetBlue\Core\Exception\SQLException;
+use EmmetBlue\Core\Exception\UndefinedValueException;
+use EmmetBlue\Core\Session\Session;
+use EmmetBlue\Core\Logger\DatabaseLog;
+use EmmetBlue\Core\Logger\ErrorLog;
+use EmmetBlue\Core\Constant;
+
+/**
+ * class ViewBody.
+ *
+ * ViewBody Controller
+ *
+ * @author 
+ * @since v0.0.1 08/06/2016 14:2016
+ */
+class ViewBody
+{ 
+	/**
+	 * viewBodyinfo method
+	 *
+	 * @param int $bodyId
+	 * @author bardeson Lucky <Ahead!!> <flashup4all@gmail.com>
+	 */
+	public static function viewBodyInfo(int $bodyId)
+	{
+		$selectBuilder = (new Builder('QueryBuilder','Select'))->getBuilder();
+		$selectBuilder
+			->columns('*')
+			->from('Mortuary.BodyInformation')
+			->where('BodyID ='.$bodyId);
+		try
+		{
+			$viewBodyOperation = (DBConnectionFactory::getConnection()->query((string)$selectBuilder))->fetchAll(\PDO::FETCH_ASSOC);
+
+			DatabaseLog::log(
+				Session::get('USER_ID'),
+				Constant::EVENT_SELECT,
+				'Mortuary',
+				'BodyInformation',
+				(string)$selectBuilder
+			);
+
+			if(count($viewBodyOperation) > 0)
+			{
+				return $viewBodyOperation;
+			}
+			else
+			{
+				return null;
+			}			
+		} 
+		catch (\PDOException $e) 
+		{
+			throw new SQLException(
+				sprintf(
+					"Error procesing request"
+				),
+				Constant::UNDEFINED
+			);
+			
+		}
+	}
+}
