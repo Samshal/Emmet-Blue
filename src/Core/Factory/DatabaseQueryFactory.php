08999547576d8e45cee4b787d1c74e32c9204ac4
Samshal <samueladeshina73@gmail.com>
2016-07-09 01:25:25 +0100

    Added lastInsertId Support

--- src/Core/Factory/DatabaseQueryFactory.php
+++ src/Core/Factory/DatabaseQueryFactory.php
@@ -50,14 +50,15 @@ class DatabaseQueryFactory
             $schemaName = $parts[0];
             $tableName = $parts[1];
 
-            $result = (
-                   DBConnectionFactory::getConnection()
-                   ->prepare((string)$query)->execute()
-               );
+            $connection = DBConnectionFactory::getConnection();
+
+            $result = $connection->prepare((string)$query)->execute();
 
             DatabaseLog::log(Session::get('USER_ID'), Constant::EVENT_INSERT, $schemaName, $tableName, $query);
 
-            return array_push($result, DBConnectionFactory::getConnection()->lastInsertId());
+            $lastInsertId = ($connection->query("SELECT CAST(COALESCE(SCOPE_IDENTITY(), @@IDENTITY) AS int) as id")->fetchColumn());
+
+            return [$result, "lastInsertId"=>$lastInsertId];
         }
         catch (\PDOException $e)
         {
