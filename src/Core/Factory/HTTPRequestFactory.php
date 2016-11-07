ddc223ad7cd5ad34624117a9b4fb752a40dad0dc
samshal <samueladeshina73@gmail.com>
2016-11-07 16:29:02 +0100

    New Factory

---
+++ src/Core/Factory/HTTPRequestFactory.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+/**
+ * @license MIT
+ * @author Samuel Adeshina <samueladeshina73@gmail.com>
+ *
+ * This file is part of the EmmetBlue project, please read the license document
+ * available in the root level of the project
+ */
+namespace EmmetBlue\Core\Factory;
+
+use Requests;
+
+/**
+ * Class HTTPRequestFactory.
+ *
+ * @author Samuel Adeshina <samueladeshina73@gmail.com>
+ *
+ * @since v0.0.1 08/06/2016 14:20
+ */
+class HTTPRequestFactory
+{
+	private static $headers;
+
+	public static function bootstrap()
+	{
+		$configJson = file_get_contents("bin/configs/http-headers-config.json");
+
+        $this->headers = json_decode($configJson);
+	}
+
+	public static function get($url, $extraHeaders = [])
+	{
+		self::bootstrap();
+		
+		return Requests::get($url, array_merge($this->headers, $extraHeaders));
+	}
+}
