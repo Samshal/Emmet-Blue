5d3c9a977bc9a897e88158776f5fc85926db55f3
samshal <samueladeshina73@gmail.com>
2016-11-19 16:49:41 +0100

    Automated Push

---
+++ src/Core/CustomFileNameGenerator.php
@@ -0,0 +1,30 @@
+<?php
+namespace EmmetBlue\Core;
+
+use Closure;
+use FileUpload\FileUpload;
+
+class CustomFileNameGenerator implements \FileUpload\FileNameGenerator\FileNameGenerator
+{
+    protected $generator;
+
+    /**
+     * @param string|callable|Closure $nameGenerator
+     */
+    public function __construct($nameGenerator)
+    {
+        $this->generator = $nameGenerator;
+    }
+
+    public function getFileName($source_name, $type, $tmp_name, $index, $content_range, FileUpload $upload)
+    {
+        if (is_string($this->generator) && !is_callable($this->generator)) {
+            return $this->generator;
+        }
+
+        return call_user_func_array(
+            $this->generator,
+            func_get_args()
+        );
+    }
+}
