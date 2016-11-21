dc8236acae07191305ec280383241aec5118b6c7
samshal <samueladeshina73@gmail.com>
2016-11-21 22:05:06 +0100

    Automated Push

--- src/Core/CustomFileNameGenerator.php
+++ src/Core/CustomFileNameGenerator.php
@@ -18,8 +18,10 @@ class CustomFileNameGenerator implements \FileUpload\FileNameGenerator\FileNameG
 
     public function getFileName($source_name, $type, $tmp_name, $index, $content_range, FileUpload $upload)
     {
+        $sourceNameArray = explode(".", $source_name);
+        $ext = $sourceNameArray[count($sourceNameArray) - 1];
         if (is_string($this->generator) && !is_callable($this->generator)) {
-            return $this->generator;
+            return $this->generator.".".$ext;
         }
 
         return call_user_func_array(
